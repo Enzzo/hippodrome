76172d50a2ac77fa8a3ab05952cbe326af18b4d2
Enzzo <enzzomatrix@gmail.com>
2024-04-16 09:02:30 +1100

    upd

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,16 +11,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class HorseTest {
 
     @Test
-    void horseConstructorTest(){
-        Horse testHorse = Mockito.mock(Horse.class);
-//        Mockito.when(new Horse(null, Mockito.anyDouble(), Mockito.anyDouble())).thenThrow(IllegalArgumentException.class);
-        Mockito.when(new Horse(null, Mockito.anyDouble())).thenThrow(IllegalArgumentException.class);
-        //        assertThrows(IllegalArgumentException.class, () -> {
-//            new Horse(null, Mockito.anyDouble(), Mockito.anyDouble());
-//        });
+    void horseConstructorNullNameExceptionTest(){
 
         assertThrows(IllegalArgumentException.class, () -> {
-            new Horse(null, Mockito.anyDouble());
+            new Horse(null, 10.0);                   
         });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Horse(null, 10.0, 10.0);   
+        });
+    }
+
+    @Test
+    void horseConstructorNullNameExceptionMessageTest(){
+        String test1 = new String();
+        String test2 = new String();
+
+        try{
+            Horse testHorse = new Horse(null, 10.0);
+        }
+        catch(IllegalArgumentException e){
+            test1 = e.getMessage();
+        }
+
+        try{
+            Horse testHorse = new Horse(null, 10.0, 10.0);
+        }
+        catch(IllegalArgumentException e){
+            test2 = e.getMessage();
+        }
+
+        assertEquals("Name cannot be null.", test1);
+        assertEquals("Name cannot be null.", test2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(names = {"", " ", "  ", "   "})
+    void horseConstructorBlankNameExceptionTest(String name){
+        
     }
 }
