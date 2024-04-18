d9f2c4be01aa723ef31b4945873d52abf9489552
Enzzo <enzzomatrix@gmail.com>
2024-04-18 17:34:08 +1100

    upd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -15,14 +19,17 @@ public class Main {
         );
         Hippodrome hippodrome = new Hippodrome(horses);
 
-        for (int i = 0; i < 100; i++) {
+        logger.warn("Начало скачек. Количество участников: {}", hippodrome.getHorses().size());
+
+        for (int i = 0; i < 10; i++) {
             hippodrome.move();
             watch(hippodrome);
-            TimeUnit.MILLISECONDS.sleep(200);
+            TimeUnit.MILLISECONDS.sleep(20);
         }
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
+        logger.warn("Окончание скачек. Победитель: " + winnerName);
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
